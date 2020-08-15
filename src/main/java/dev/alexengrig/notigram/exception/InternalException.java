01bd0eb93fb00368c721faa7ba8f15ab5ba3a5e1
Grig Alex <alexengrigdev@gmail.com>
2020-08-15 21:57:16 +0300

    Create InternalException

---
+++ src/main/java/dev/alexengrig/notigram/exception/InternalException.java
@@ -0,0 +1,7 @@
+package dev.alexengrig.notigram.exception;
+
+public class InternalException extends RuntimeException {
+    public InternalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
